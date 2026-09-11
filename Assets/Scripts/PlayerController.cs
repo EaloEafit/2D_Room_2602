40022de33a890409f7b1c0b3fa9dd5845cb64cc4
EaloEafit <lcealoo@eafit.edu.co>
2026-09-11 07:20:47 -0500

    Player controller start

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       // Leemos la entrada del teclado WASD o Flechas
+       float moveHorizontal = Input.GetAxis("Horizontal");
+       float moveVertical = Input.GetAxis("Vertical");
+
+
+
+
+
     }
 }
